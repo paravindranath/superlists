e4703d65d7379b8e0986cf68b64b96306c6b65c5
Pradeep Ravindranath <pradeepanand.r@gmail.com>
2016-12-13 17:49:35 -0800

    First commit - first fn test and django basic config

---
+++ functional_tests.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+browser = webdriver.Firefox()#firefox_binary=FirefoxBinary(
+    #firefox_path='/Applications/Firefoxesr.app'))
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
