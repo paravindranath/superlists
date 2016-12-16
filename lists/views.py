a8adecb9fc13d831dddb49cc8b4986e246fe23ed
Pradeep Ravindranath <pradeepanand.r@gmail.com>
2016-12-15 17:44:57 -0800

    Three Strikes and refactor 5.4 flawed redundant

--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return render(request,'home.html')# HttpResponse('<html><title>To-Do lists</title></html>')
+    #if request.method == 'POST':
+    #    return HttpResponse(request.POST['item_text'])
+    return render(request,'home.html',{
+        'new_item_text': request.POST.get('item_text',''),})# HttpResponse('<html><title>To-Do lists</title></html>')
 
