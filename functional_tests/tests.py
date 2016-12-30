9afdf62cfe8786be253231102d44166b133ea9d3
Pradeep Ravindranath <pradeepanand.r@gmail.com>
2016-12-29 16:33:02 -0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 #from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-class NewVisitorTest(LiveServerTestCase):  
+class NewVisitorTest(StaticLiveServerTestCase):  
 
     def setUp(self):  
         self.browser = webdriver.Firefox()
@@ -111,23 +111,23 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-
+        ## import pdb; pdb.set_trace()
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5
-        )
+        #import pdb; pdb.set_trace()
+        self.assertAlmostEqual( inputbox.location['x'] + (inputbox.size['width']/2),
+                                512,
+                                delta=5
+                                )
         # She starts a new list and sees the input is nicely
         # centered there too
         inputbox.send_keys('testing\n')
+        #import pdb; pdb.set_trace()
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5
-        )
+        self.assertAlmostEqual( inputbox.location['x'] + inputbox.size['width']/2,
+                                512,
+                                delta=5
+                                )
 
         
         # Edith wonders whether the site will remember her list. Then she sees
